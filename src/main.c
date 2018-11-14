c72e7eb8cd8e6d423dc4753683fe2c7fd2e23263
pangelisge1 <44798364+pangelisge1@users.noreply.github.com>
2018-11-14 19:05:20 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,45 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <math.h>
+#include "stdlib.h"
 
-#include "my_header.h"
+#define RAND_MAX 32767
 
 int main(void)
 {
-    return 0;
+	int n=1;
+	long S1=0;
+	
+	int a = ((rand())%10)+1;
+	int i = 0;
+	int sum = 0;
+	
+	int c = 1;
+	int b = 0;
+	int dam = 0;
+
+	
+	while (S1<9876){
+		
+		S1 = S1 + 3 * n;
+		n = n+1;
+	}
+	
+	while (i<12){
+		
+		sum = sum + a;
+		i = i + 1;
+		a = ((rand())%10)+1;
+	}
+	
+	while (b<20){
+		
+		if (c%3==0 || c%7==0){
+			dam = dam + c;
+			c = c + 1;
+			b = b+1;
+		}
+		else
+			c = c+1;
+	}
+	
+	return 0;
 }
