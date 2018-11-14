2284d497b1d6b2aca1abad24b12ad10f062add0f
pangelisge1 <44798364+pangelisge1@users.noreply.github.com>
2018-11-14 23:37:56 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,45 +1,30 @@
-#include <math.h>
-#include "stdlib.h"
+#include <TP_lib.h>
 
-#define RAND_MAX 32767
-
-int main(void)
-{
-	int n=1;
-	long S1=0;
+int main(void){
 	
-	int a = ((rand())%10)+1;
-	int i = 0;
-	int sum = 0;
+	short int LancerDe (void);
+	short res;
+	long val=0;
 	
-	int c = 1;
-	int b = 0;
-	int dam = 0;
-
-	
-	while (S1<9876){
+	do {
+		res = LancerDe();
 		
-		S1 = S1 + 3 * n;
-		n = n+1;
-	}
-	
-	while (i<12){
+		if (res == 2)
+			val= val+2;
 		
-		sum = sum + a;
-		i = i + 1;
-		a = ((rand())%10)+1;
-	}
-	
-	while (b<20){
+		else if (res == 4)
+			val= val+4;
 		
-		if (c%3==0 || c%7==0){
-			dam = dam + c;
-			c = c + 1;
-			b = b+1;
-		}
-		else
-			c = c+1;
-	}
+		else if (res == 6)
+			val= val+6;
+		
+		else if (res == 3)
+			val= val*2;
+		
+		else if (res == 5)
+			val= val-2;
+		
+	}while(res!=1);
 	
 	return 0;
 }
