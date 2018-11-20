8c1e44e5ceababea0ed67276c182de5ea35410ad
pangelisge1 <44798364+pangelisge1@users.noreply.github.com>
2018-11-20 13:36:14 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
+#include <ansi_c.h>
 #include <math.h>
 
 #define SEUIL 26
+#define SEUILB -100
+#define SEUILH 200
 
 int main(void)
 {
 	unsigned char bin[10] = {17, 19, 21, 23, 25, 27, 29, 31, 33, 35};
 	char i;
+	int sat[10] = {rand()-16384, rand()-16384, rand()-16384, rand()-16384, rand()-16384, rand()-16384, rand()-16384, rand()-16384, rand()-16384, rand()-16384};
+	char h;
 	
 	for(i=0; i<SEUIL; i++)
 	{
@@ -16,5 +21,22 @@ int main(void)
 			bin[i] = 1;
 	}
 	
+	for(h=0; h<SEUILB; h++)
+	{
+		if(sat[h] < SEUILB)
+			sat[h] = SEUILB;
+		
+		else
+			sat[h] = sat[h];
+	}
+	
+	for (h=0; h>SEUILH; h++)
+	{
+		if(sat[h] > SEUILH)
+			sat[h] = SEUILH;
+		
+		else
+			sat[h] = sat[h];
+	}
 	return 0;
 }
