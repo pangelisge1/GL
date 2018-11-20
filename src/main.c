d6ffeb57a50c6ee47cb283b1778a0fec5b56b2cd
pierre angelis <pierre.angelis@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:47:39 +0100

    jai fini la partie 1 de l'exo 56

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <math.h>
 
-#include "my_header.h"
+#define SEUIL 26
 
 int main(void)
 {
+	unsigned char bin[10] = {17, 19, 21, 23, 25, 27, 29, 31, 33, 35};
+	char i;
+	
+	for(i=0; i<SEUIL; i++)
+	{
+		if(bin[i] < SEUIL)
+			bin[i] = 0;
+		
+		else
+			bin[i] = 1;
+	}
+	
+	return 0;
 }
