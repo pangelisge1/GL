e2a809123c97c4e34f97bc3a6afbfd4c814cebda
pangelisge1 <44798364+pangelisge1@users.noreply.github.com>
2018-11-27 21:39:09 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,25 +1,42 @@
-#include <stdlib.h>
-#include "TP_lib_suite.h"
-
+#include "TP_lib_suite.h" 
 
 int main(void)
 {
-	unsigned char joueur[6] = {30, 12, 2, 21, 5, 16};
-	unsigned char gagnant[6] = {27, 18, 7, 3, 41, 13};
-	unsigned char i, j;
-	int g=0;
-	void initialiserTirage();
-	short tirerNumero();
-	
-	for (j=0; j<N; j++)
-	{
-		for (i=0; i<N; i++)
+     	 initialiserTirage();
+	 unsigned char j, i, nc = 0, t = 0, score = 0;
+	 unsigned char tirage[6];
+	 unsigned char joueur[6];
+	 
+	 for(j=0; j<6; j++)
+	 {
+		user[j] = tirerNumero();	 
+	 }
+	 
+	 for(t=0; t<100; t++) 
+	 {
+		 nc = 0;
+	 	initialiserTirage();
+		
+		for(i=0; i<6; i++)
+	 	{
+			tirage[i] = tirerNumero();	 
+		}
+	 	
+		
+		for(j=0; j<6; j++)
+	 	{
+	   		for (i=0; i<6; i++)  
+	  		{	   
+	   			if (user[j] == tirage[i])
+				{
+			  		nc++;
+				}	
+	   		}
+	 	}
+		if (nc > score)
 		{
-			if (gagnante[j] == joueur[i])
-				g++;
-	
-		}					  
+			score = nc;	
+		}	
 	}
-	
-    return 0;
+	return 0;
 }
