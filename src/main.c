74c70deb1e99ef8f29f9c0fe430fec100811fa8d
pangelisge1 <44798364+pangelisge1@users.noreply.github.com>
2018-11-27 19:24:09 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
-#include "my_header.h"
+#include <stdlib.h>
+#include "TP_lib_suite.h"
 
 int main(void)
 {
+	unsigned char joueur[6] = {30, 12, 2, 21, 5, 16};
+	unsigned char gagnant[6] = {27, 18, 7, 3, 41, 13};
+	unsigned char i, j;
+	int g=0;
+    
+	for (j=0; j<N; j++)
+	{
+		for (i=0; i<N; i++)
+		{
+			if (gagnante[j] == joueur[i])
+				g++;
+	
+		}					  
+	}
+	
     return 0;
 }
