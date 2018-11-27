7df4cc6907ba26f0b86abf27dd643d1fa04a928b
pangelisge1 <44798364+pangelisge1@users.noreply.github.com>
2018-11-27 20:58:57 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 #include <stdlib.h>
 #include "TP_lib_suite.h"
 
+
 int main(void)
 {
 	unsigned char joueur[6] = {30, 12, 2, 21, 5, 16};
 	unsigned char gagnant[6] = {27, 18, 7, 3, 41, 13};
 	unsigned char i, j;
 	int g=0;
-    
+	void initialiserTirage();
+	short tirerNumero();
+	
 	for (j=0; j<N; j++)
 	{
 		for (i=0; i<N; i++)
